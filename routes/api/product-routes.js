b8be49c98b1028568ec2449d88ea97237f10d2e1
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-25 14:14:18 -0400

    add delete  api for Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -43,12 +43,12 @@ router.get("/:id", async (req, res) => {
 // create new product
 router.post("/", (req, res) => {
   /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
+{
+	 "product_name": "Basketball",
+   "price": 200.00,
+   "stock": 3,
+   "tagIds": [1, 2, 3, 4]
+}
     
   */
 
@@ -116,8 +116,24 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id",async  (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(400).json({ message: "No product found with that id!" });
+      return;
+    }
+
+    res.status(200).json( productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
